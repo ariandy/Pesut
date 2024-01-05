58f08526d71e6f27102d6a531ecb34e79be0e7bb
ariandy <ariandylinux@gmail.com>
2024-01-05 11:55:24 +0700

    Struktur token

--- compiler.h
+++ compiler.h
@@ -2,6 +2,48 @@
 #define PEACHCOMPILER_H
 
 #include <stdio.h>
+#include <stdbool.h>
+
+struct pos
+{
+    int line;
+    int col;
+    const char* filename;
+};
+
+enum
+{
+    TOKEN_TYPE_IDENTIFIER,
+    TOKEN_TYPE_KEYWORD,
+    TOKEN_TYPE_OPERATOR,
+    TOKEN_TYPE_SYMBOL,
+    TOKEN_TYPE_NUMBER,
+    TOKEN_TYPE_STRING,
+    TOKEN_TYPE_COMMENT,
+    TOKEN_TYPE_NEWLINE
+};
+
+struct token
+{
+    int type;
+    int flags;
+
+    union
+    {
+        char cval;
+        const char* sval;
+        unsigned int inum;
+        unsigned long lnum;
+        unsigned long long llnum;
+        void* any;
+    };
+
+    // Akan bernilai True apabila ada whitespace di antara token dengan token selanjutnya
+    bool whitespace;
+
+    const char* between_brackets;
+
+};
 
 enum
 {
