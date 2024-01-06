d7912068bf93e344adb07d5420a50108fb73509a
ariandy <ariandylinux@gmail.com>
2024-01-06 22:20:17 +0700

    Test untuk token angka

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include "vector.h"
+#include "compiler.h"
 int main()
 {
-    printf("Hello World!\n");
+    int res = compile_file("./test.c", "./test", 0);
+    if (res == COMPILER_FILE_COMPILED_OK)
+    {
+        printf("everything compiled file\n");
+    }
+    else if(res == COMPILER_FAILED_WITH_ERRORS)
+    {
+        printf("Compile failed\n");
+    }
+    else
+    {
+        printf("Unknown response for compile time\n");
+    }
     return 0;
 }
